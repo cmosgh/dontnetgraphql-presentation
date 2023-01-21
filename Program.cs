9cb18efdfe3dc8e7edd148d885b01a8ba563e8dc
Cristian Mos <cmo@nemetos.com>
2023-01-21 14:47:42 +0200

    hello world

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using GraphQL;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddGraphQL(b => b
+    .AddAutoSchema<Query>() // schema
+    .AddSystemTextJson()); // serializer
+
+var app = builder.Build();
+app.UseDeveloperExceptionPage();
+app.UseWebSockets();
+app.UseGraphQL(); // default path is /graphql
+app.UseGraphQLPlayground(
+    "/graphqli", // url to host Playground at
+    new GraphQL.Server.Ui.Playground.PlaygroundOptions
+    {
+        GraphQLEndPoint = "/graphql", // url of GraphQL endpoint
+        SubscriptionsEndPoint = "/graphql", // url of GraphQL endpoint
+    });
+await app.RunAsync();
+
+public class Query
+{
+    public static string hello(string? name = "World") => $"Hello {name}!";
+}
